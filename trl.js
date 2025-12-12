bd88cc7052c3d4b93214641394b83ebaf4085dee
juliandavidfloresmendoza-collab <juliandavidfloresmendoza@gmail.com>
2025-12-11 22:00:08 -0500

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1448696338995740682/o6HKx_9vzuc1b5Om-bDyMZiFb37zMfX7EyDXrzzWHMJrXGWdnjTYM3a5mmleEHnGIylz'; // Reemplaza esto con tu Webhook real
